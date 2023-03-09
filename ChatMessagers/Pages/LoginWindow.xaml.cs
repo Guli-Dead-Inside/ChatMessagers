ab00dc441b38aeaea23af036066e34c858dbd094
Гизетдинов Искандер <gizetiskander@gmail.com>
2023-03-09 16:24:14 +0300

    1

--- ChatMessagers/Pages/LoginWindow.xaml.cs
+++ ChatMessagers/Pages/LoginWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace ChatMessagers.Pages
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
