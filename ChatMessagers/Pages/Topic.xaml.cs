c1f54d7d82ac873b244208a48d2bca16ec7fc2ce
булат шакиров <a2009-1@mail.ru>
2023-03-09 15:44:00 +0300

    all visual end

---
+++ ChatMessagers/Pages/Topic.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChatMessagers.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Topic.xaml
+    /// </summary>
+    public partial class Topic : Window
+    {
+        public Topic()
+        {
+            InitializeComponent();
+        }
+    }
+}
